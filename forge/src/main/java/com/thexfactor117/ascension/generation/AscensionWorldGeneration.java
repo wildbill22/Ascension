5b266c6b08a1b90071d4ae47a7988c277b633389
TheXFactor117 <thexfactor117.mods@gmail.com>
2014-11-13 14:17:28 -0600

    Commit-4: en_US.lang Updated
    Along with:
    * Updated mcmod.info file
    * Cleaned up World Generation

--- forge/src/main/java/com/thexfactor117/ascension/generation/AscensionWorldGeneration.java
+++ forge/src/main/java/com/thexfactor117/ascension/generation/AscensionWorldGeneration.java
@@ -16,14 +16,14 @@ public class AscensionWorldGeneration implements IWorldGenerator
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
 	{
 		switch (world.provider.dimensionId)
-    {
-    	case 0:
-    		generateSurface(world, random, chunkX * 16, chunkZ * 16);
-    	case -1:
-    		generateNether(world, random, chunkX * 16, chunkZ * 16);
-    	case 1:
-    		generateEnd(world, random, chunkX * 16, chunkZ * 16);
-    }
+		{
+    		case 0:
+    			generateSurface(world, random, chunkX * 16, chunkZ * 16);
+    		case -1:
+    			generateNether(world, random, chunkX * 16, chunkZ * 16);
+    		case 1:
+    			generateEnd(world, random, chunkX * 16, chunkZ * 16);
+		}
 	}
 
 	private void generateSurface(World world, Random random, int x, int z)
@@ -45,11 +45,11 @@ public class AscensionWorldGeneration implements IWorldGenerator
 	private void addOreSpawn(Block block, World world, Random random, int blockXPos, int blockZPos, int maxX, int maxZ, int maxVeinSize, int chanceToSpawn, int minY, int maxY)
 	{
 		for (int i = 0; i < chanceToSpawn; i++)
-    {
+		{
 			int posX = blockXPos + random.nextInt(maxX);
 			int posY = minY + random.nextInt(maxY - minY);
 			int posZ = blockZPos + random.nextInt(maxZ);
 			new WorldGenMinable(block, maxVeinSize).generate(world, random, posX, posY, posZ);
-    }
+		}
 	}
 }
