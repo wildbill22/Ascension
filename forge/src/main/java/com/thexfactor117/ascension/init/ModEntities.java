e5a3af05bc01523af92a6e4020fc7b0591310ae4
TheXFactor117 <thexfactor117.mods@gmail.com>
2015-01-09 14:55:57 -0600

    Added mob spawning
    Plus a few textures

--- forge/src/main/java/com/thexfactor117/ascension/init/ModEntities.java
+++ forge/src/main/java/com/thexfactor117/ascension/init/ModEntities.java
@@ -1,5 +1,8 @@
 package com.thexfactor117.ascension.init;
 
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.world.biome.BiomeGenBase;
+
 import com.thexfactor117.ascension.entities.EntityBarbarian;
 import com.thexfactor117.ascension.entities.EntityBear;
 import com.thexfactor117.ascension.entities.EntityGhost;
@@ -9,6 +12,8 @@ import com.thexfactor117.ascension.entities.EntityMummy;
 import com.thexfactor117.ascension.entities.EntitySnowBlaze;
 import com.thexfactor117.ascension.entities.EntityValkyrie;
 import com.thexfactor117.ascension.handlers.EntityHandler;
+
+import cpw.mods.fml.common.registry.EntityRegistry;
 
 public class ModEntities 
 {
@@ -24,5 +29,20 @@ public class ModEntities
 		EntityHandler.registerMonsters(EntityBear.class, "Bear");
 		//EntityHandler.registerMonsters(EntityShark.class, "Shark");
 		EntityHandler.registerMonsters(EntityGhost.class, "Ghost");
+		
+		addMobSpawns();
+	}
+	
+	public static void addMobSpawns()
+	{
+		EntityRegistry.addSpawn(EntityMummy.class, 1, 1, 2, EnumCreatureType.monster, BiomeGenBase.desert);
+		EntityRegistry.addSpawn(EntityValkyrie.class, 1, 1, 3, EnumCreatureType.monster, BiomeGenBase.forest);
+		EntityRegistry.addSpawn(EntityGolem.class, 1, 1, 1, EnumCreatureType.monster, BiomeGenBase.stoneBeach);
+		//EntityRegistry.addSpawn(EntityGoblin.class, 1, 1, 3, EnumCreatureType.monster, BiomeGenBase.swampland);
+		EntityRegistry.addSpawn(EntityBarbarian.class, 2, 2, 4, EnumCreatureType.monster, BiomeGenBase.plains);
+		//EntityRegistry.addSpawn(EntitySnowBlaze.class, 1, 1, 2, EnumCreatureType.monster, BiomeGenBase.coldTaiga);
+		EntityRegistry.addSpawn(EntityBear.class, 1, 1, 2, EnumCreatureType.monster, BiomeGenBase.forest);
+		//EntityRegistry.addSpawn(EntityShark.class, 1, 1, 2, EnumCreatureType.monster, BiomeGenBase.ocean);
+		//EntityRegistry.addSpawn(EntityGhost.class, 1, 1, 1, EnumCreatureType.monster, BiomeGenBase.jungle);
 	}
 }
