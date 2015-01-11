b0e5aada322adfd9569b35948435587a514d6050
TheXFactor117 <thexfactor117.mods@gmail.com>
2015-01-10 20:05:27 -0600

    Changed probability at which mobs spawn

--- forge/src/main/java/com/thexfactor117/ascension/init/ModEntities.java
+++ forge/src/main/java/com/thexfactor117/ascension/init/ModEntities.java
@@ -35,13 +35,13 @@ public class ModEntities
 	
 	public static void addMobSpawns()
 	{
-		EntityRegistry.addSpawn(EntityMummy.class, 1, 1, 2, EnumCreatureType.monster, BiomeGenBase.desert, BiomeGenBase.desertHills);
-		EntityRegistry.addSpawn(EntityValkyrie.class, 1, 1, 3, EnumCreatureType.monster, BiomeGenBase.forest, BiomeGenBase.birchForest, BiomeGenBase.birchForestHills, BiomeGenBase.forestHills, BiomeGenBase.jungle, BiomeGenBase.jungleEdge, BiomeGenBase.jungleHills, BiomeGenBase.roofedForest);
-		EntityRegistry.addSpawn(EntityGolem.class, 1, 1, 1, EnumCreatureType.monster, BiomeGenBase.stoneBeach, BiomeGenBase.extremeHills, BiomeGenBase.extremeHillsEdge, BiomeGenBase.extremeHillsPlus);
-		//EntityRegistry.addSpawn(EntityGoblin.class, 1, 1, 3, EnumCreatureType.monster, BiomeGenBase.swampland);
-		EntityRegistry.addSpawn(EntityBarbarian.class, 2, 2, 4, EnumCreatureType.monster, BiomeGenBase.plains, BiomeGenBase.desert, BiomeGenBase.extremeHills, BiomeGenBase.extremeHillsEdge, BiomeGenBase.extremeHillsPlus, BiomeGenBase.desert, BiomeGenBase.desertHills, BiomeGenBase.iceMountains, BiomeGenBase.icePlains, BiomeGenBase.mesa, BiomeGenBase.mesaPlateau, BiomeGenBase.mesaPlateau_F, BiomeGenBase.mushroomIsland, BiomeGenBase.mushroomIslandShore, BiomeGenBase.savanna, BiomeGenBase.savannaPlateau, BiomeGenBase.swampland, BiomeGenBase.taiga, BiomeGenBase.taigaHills);
+		EntityRegistry.addSpawn(EntityMummy.class, 10, 1, 3, EnumCreatureType.monster, BiomeGenBase.desert, BiomeGenBase.desertHills);
+		EntityRegistry.addSpawn(EntityValkyrie.class, 100, 2, 6, EnumCreatureType.monster, BiomeGenBase.forest, BiomeGenBase.birchForest, BiomeGenBase.birchForestHills, BiomeGenBase.forestHills, BiomeGenBase.jungle, BiomeGenBase.jungleEdge, BiomeGenBase.jungleHills, BiomeGenBase.roofedForest);
+		EntityRegistry.addSpawn(EntityGolem.class, 25, 1, 2, EnumCreatureType.monster, BiomeGenBase.stoneBeach, BiomeGenBase.extremeHills, BiomeGenBase.extremeHillsEdge, BiomeGenBase.extremeHillsPlus);
+		//EntityRegistry.addSpawn(EntityGoblin.class, 100, 1, 3, EnumCreatureType.monster, BiomeGenBase.swampland);
+		EntityRegistry.addSpawn(EntityBarbarian.class, 100, 2, 6, EnumCreatureType.monster, BiomeGenBase.plains, BiomeGenBase.desert, BiomeGenBase.extremeHills, BiomeGenBase.extremeHillsEdge, BiomeGenBase.extremeHillsPlus, BiomeGenBase.desert, BiomeGenBase.desertHills, BiomeGenBase.iceMountains, BiomeGenBase.icePlains, BiomeGenBase.mesa, BiomeGenBase.mesaPlateau, BiomeGenBase.mesaPlateau_F, BiomeGenBase.mushroomIsland, BiomeGenBase.mushroomIslandShore, BiomeGenBase.savanna, BiomeGenBase.savannaPlateau, BiomeGenBase.swampland, BiomeGenBase.taiga, BiomeGenBase.taigaHills);
 		//EntityRegistry.addSpawn(EntitySnowBlaze.class, 1, 1, 2, EnumCreatureType.monster, BiomeGenBase.coldTaiga);
-		EntityRegistry.addSpawn(EntityBear.class, 1, 1, 2, EnumCreatureType.monster, BiomeGenBase.forest, BiomeGenBase.birchForest, BiomeGenBase.birchForestHills, BiomeGenBase.forestHills, BiomeGenBase.jungle, BiomeGenBase.jungleEdge, BiomeGenBase.jungleHills, BiomeGenBase.roofedForest, BiomeGenBase.taiga, BiomeGenBase.taigaHills);
+		EntityRegistry.addSpawn(EntityBear.class, 50, 2, 4, EnumCreatureType.monster, BiomeGenBase.forest, BiomeGenBase.birchForest, BiomeGenBase.birchForestHills, BiomeGenBase.forestHills, BiomeGenBase.jungle, BiomeGenBase.jungleEdge, BiomeGenBase.jungleHills, BiomeGenBase.roofedForest, BiomeGenBase.taiga, BiomeGenBase.taigaHills);
 		//EntityRegistry.addSpawn(EntityShark.class, 1, 1, 2, EnumCreatureType.monster, BiomeGenBase.ocean);
 		//EntityRegistry.addSpawn(EntityGhost.class, 1, 1, 1, EnumCreatureType.monster, BiomeGenBase.jungle);
 	}
