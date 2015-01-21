ab6cf80d9b64ff91302fefcaed866eebb5d5f52c
TheXFactor117 <thexfactor117.mods@gmail.com>
2015-01-21 12:01:54 -0600

    Added the Ghost (temporary most likely)

--- forge/src/main/java/com/thexfactor117/ascension/entities/hostile/EntityGhost.java
+++ forge/src/main/java/com/thexfactor117/ascension/entities/hostile/EntityGhost.java
@@ -1,10 +1,14 @@
 package com.thexfactor117.ascension.entities.hostile;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -19,10 +23,14 @@ public class EntityGhost extends EntityMob
 		super(world);
 		this.getNavigator().setCanSwim(true);
 		this.experienceValue = 20;
-		this.setSize(1.5F, 1.0F); //sets how big the hit box is *** TEST THIS ***
-		this.tasks.addTask(0, new EntityAIWander(this, 1.0D)); //speed at which mob wanders
-		this.tasks.addTask(1, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false)); // attacks player once collided
-		this.tasks.addTask(2, new EntityAISwimming(this));
+		this.setSize(1.5F, 1.0F);
+		this.clearAITasks();
+		this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, true));
+        this.tasks.addTask(3, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+		this.tasks.addTask(4, new EntityAIWander(this, 1.0D));
+		this.tasks.addTask(5, new EntityAISwimming(this));
+        this.targetTasks.addTask(0, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
 	}
 	
 	@Override
@@ -31,16 +39,49 @@ public class EntityGhost extends EntityMob
 		super.applyEntityAttributes();
 		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(5.0D);
 		this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(16.0D);
-		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.15D);
+		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.175D);
 		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(30.0D);
 	}
 	
+	/**
+	 * Overrides to make sure the AI is enabled
+	 */
+	@Override
 	public boolean isAIEnabled()
 	{
 		return true;
 	}
 	
-	public boolean hitEntity(ItemStack item, EntityLivingBase target, EntityLivingBase attacker)
+	/**
+	 * Clears previous AI Tasks, so the ones defined above will
+	 * actually perform.
+	 */
+	protected void clearAITasks()
+	{
+		tasks.taskEntries.clear();
+		targetTasks.taskEntries.clear();
+	}
+	
+	/**
+	 * Testing purposes. Entities are having trouble attacking
+	 * players; hopefully this will help out.
+	 */
+    @Override
+	protected Entity findPlayerToAttack()
+    {
+        EntityPlayer entityplayer = this.worldObj.getClosestVulnerablePlayerToEntity(this, 24.0D);
+        return entityplayer != null && this.canEntityBeSeen(entityplayer) ? entityplayer : null;
+    }
+	
+    /**
+     * Ghost turns invisible once hit for a period of 2 or so seconds.
+     * 
+     * @param item
+     * @param target
+     * @param attacker
+     * @return
+     */
+    public boolean hitEntity(ItemStack item, EntityLivingBase target, EntityLivingBase attacker)
 	{
 		item.damageItem(1, attacker);
 		
--- forge/src/main/java/com/thexfactor117/ascension/proxies/ClientProxy.java
+++ forge/src/main/java/com/thexfactor117/ascension/proxies/ClientProxy.java
@@ -1,17 +1,20 @@
 package com.thexfactor117.ascension.proxies;
 
 import com.thexfactor117.ascension.entities.hostile.EntityBarbarian;
+import com.thexfactor117.ascension.entities.hostile.EntityGhost;
 import com.thexfactor117.ascension.entities.hostile.EntityGolem;
 import com.thexfactor117.ascension.entities.hostile.EntityMummy;
 import com.thexfactor117.ascension.entities.hostile.EntityValkyrie;
 import com.thexfactor117.ascension.entities.passive.EntityDoe;
 import com.thexfactor117.ascension.models.ModelBarbarian;
 import com.thexfactor117.ascension.models.ModelDoe;
+import com.thexfactor117.ascension.models.ModelGhost;
 import com.thexfactor117.ascension.models.ModelGolem;
 import com.thexfactor117.ascension.models.ModelMummy;
 import com.thexfactor117.ascension.models.ModelValkyrie;
 import com.thexfactor117.ascension.render.RenderBarbarian;
 import com.thexfactor117.ascension.render.RenderDoe;
+import com.thexfactor117.ascension.render.RenderGhost;
 import com.thexfactor117.ascension.render.RenderGolem;
 import com.thexfactor117.ascension.render.RenderMummy;
 import com.thexfactor117.ascension.render.RenderValkyrie;
@@ -27,6 +30,7 @@ public class ClientProxy extends CommonProxy
 		RenderingRegistry.registerEntityRenderingHandler(EntityBarbarian.class, new RenderBarbarian(new ModelBarbarian(), 0));
 		RenderingRegistry.registerEntityRenderingHandler(EntityGolem.class, new RenderGolem(new ModelGolem(), 0));
 		RenderingRegistry.registerEntityRenderingHandler(EntityMummy.class, new RenderMummy(new ModelMummy(), 0));
+		RenderingRegistry.registerEntityRenderingHandler(EntityGhost.class, new RenderGhost(new ModelGhost(), 0));
 		
 		RenderingRegistry.registerEntityRenderingHandler(EntityDoe.class, new RenderDoe(new ModelDoe(), 0));
 	}
