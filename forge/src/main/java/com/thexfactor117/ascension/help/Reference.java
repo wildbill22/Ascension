16c3b7f9c68f07f03cbce6714cbdbceef72be924
TheXFactor117 <thexfactor117.mods@gmail.com>
2015-01-26 13:18:44 -0600

    Updating mob spawning properties
    Increasing the amount of biomes certain mobs spawn.

--- forge/src/main/java/com/thexfactor117/ascension/help/Reference.java
+++ forge/src/main/java/com/thexfactor117/ascension/help/Reference.java
@@ -4,5 +4,5 @@ public class Reference
 {
 	public static final String MODID = "thexfactor117_ascension";
 	public static final String NAME = "Ascension";
-	public static final String VERSION = "pre-a1.0.0";
+	public static final String VERSION = "a1.0.1";
 }
