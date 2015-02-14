43f15ad20ee541efd59ffb64a91b7063939610bb
wildbill22 <billwerth2@msn.com>
2015-02-14 11:13:06 -0800

    Added code to find unique entity ID

--- forge/src/main/java/com/thexfactor117/ascension/init/ModBosses.java
+++ forge/src/main/java/com/thexfactor117/ascension/init/ModBosses.java
@@ -5,13 +5,14 @@ import com.thexfactor117.ascension.entities.boss.EntityPharaoh;
 import com.thexfactor117.ascension.help.LogHelper;
 import com.thexfactor117.ascension.items.AscensionMonsterPlacer;
 
+import net.minecraft.entity.EntityList;
 import net.minecraft.item.Item;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ModBosses
 {
-    static int entityId = -1;
+    static int startEntityId = 300;
     
     public static Item itemSpawnEgg = new AscensionMonsterPlacer("Pharaoh", 0xC9C9BB, 0x182ED6).setUnlocalizedName("spawn_egg_" + "Pharaoh".toLowerCase()).setTextureName("thexfactor117_ascension:spawn_egg");
     
@@ -22,9 +23,10 @@ public class ModBosses
     
     public static void registerModEntity(Class parEntityClass, String parEntityName)
     {
-        EntityRegistry.registerModEntity(parEntityClass, parEntityName, ++entityId, Ascension.instance, 64, 1, false);
+    	int id = getUniqueEntityId();
+        EntityRegistry.registerModEntity(parEntityClass, parEntityName, id, Ascension.instance, 64, 1, false);
       	// DEBUG
-      	LogHelper.info("Registering mod entity " + parEntityName + " with ID = " + entityId);
+      	LogHelper.info("Registering mod entity " + parEntityName + " with ID = " + id);
     }
 
     public static void registerModEntityWithEgg(Class parEntityClass, String parEntityName, int parEggColor, int parEggSpotsColor)
@@ -37,5 +39,16 @@ public class ModBosses
     {
     	//Item itemSpawnEgg = new AscensionMonsterPlacer(parSpawnName, parEggColor, parEggSpotsColor).setUnlocalizedName("spawn_egg_" + parSpawnName.toLowerCase()).setTextureName("thexfactor117_ascension:spawn_egg");
     	GameRegistry.registerItem(itemSpawnEgg, "spawnEgg" + parSpawnName);
+    }
+    
+    public static int getUniqueEntityId()
+    {
+    	do
+    	{
+    		startEntityId++;
+    	}
+    	while (EntityList.getStringFromID(startEntityId) != null);
+
+    	return startEntityId;
     }
 }
