73e70a3f623e29bd8d81eb7bd52b44e37ef2eb96
TheXFactor117 <thexfactor117.mods@gmail.com>
2015-02-16 14:14:06 -0600

    Finished up Wooden Staff charging code

--- forge/src/main/java/com/thexfactor117/ascension/items/weapons/ItemWoodenStaff.java
+++ forge/src/main/java/com/thexfactor117/ascension/items/weapons/ItemWoodenStaff.java
@@ -1,16 +1,16 @@
 package com.thexfactor117.ascension.items.weapons;
 
 import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
 import com.thexfactor117.ascension.entities.projectiles.EntityMagicBall;
-import com.thexfactor117.ascension.help.LogHelper;
 import com.thexfactor117.ascension.help.Reference;
-import com.thexfactor117.ascension.init.ModArmory;
 import com.thexfactor117.ascension.init.ModItems;
 import com.thexfactor117.ascension.tabs.ModTabs;
 
@@ -19,8 +19,9 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemWoodenStaff extends Item
 {
-	private int ticks;
-	boolean inUse;
+	public static final String[] woodenStaffChargeArray = new String[] {"charging_0", "charging_1", "charging_full"};
+	@SideOnly(Side.CLIENT)
+	private IIcon[] iconArray;
 	
 	public ItemWoodenStaff()
 	{
@@ -33,88 +34,72 @@ public class ItemWoodenStaff extends Item
 	
 	@Override
 	@SideOnly(Side.CLIENT)
-	public void registerIcons(IIconRegister par1IconRegister)
+	public void registerIcons(IIconRegister iconRegister)
 	{
-		this.itemIcon = par1IconRegister.registerIcon(Reference.MODID + ":" + getUnlocalizedName().substring(5));
+		this.itemIcon = iconRegister.registerIcon(Reference.MODID + ":" + getUnlocalizedName().substring(5));
+		this.iconArray = new IIcon[woodenStaffChargeArray.length];
+		
+		for (int i = 0; i < this.iconArray.length; ++i)
+		{
+			this.iconArray[i] = iconRegister.registerIcon(this.getIconString() + "_" + woodenStaffChargeArray[i]);
+		}
 	}
 	
-	/**
-	 * Basically creates a rapid fire staff - not necessarily what
-	 * is wanted. Experimental.
-	 */
-	public void chargeStaff(ItemStack stack, World world, EntityPlayer player, int ticksLeft)
+	@SideOnly(Side.CLIENT)
+	public IIcon getItemIconForUseDuration(int i)
 	{
+		return this.iconArray[i];
+	}
+	
+	@Override
+	public void onPlayerStoppedUsing(ItemStack stack, World world, EntityPlayer player, int i)
+	{
+		int x = getMaxItemUseDuration(stack) - i;
+		float y = x / 20.0F;
+		y = (y * y + y * 2.0F) / 3.0F;
+		
+		if (y < 0.1F) return;
+		if (y > 1.0F)
+		{
+			y = 1.0F;
+		}
+		
 		if (player.capabilities.isCreativeMode || player.inventory.hasItem(ModItems.crystalShard))
 		{
-			//starts at max item use duration and is decremented each tick
-			//ticksLeft is assigned in onUpdate
-			int durationTotal = this.getMaxItemUseDuration(stack) - ticksLeft;
-			//converts ticks to seconds
-			float durationInSeconds = (float) durationTotal / 20.0F;
-			//
-			durationInSeconds = (durationInSeconds * durationInSeconds + durationInSeconds * 2.0F) / 3.0F;
-
-			if (durationInSeconds < 3.0F)
-			{	
-				LogHelper.info("Duration in seconds is too short!");
-				return;
-			}
-			else if (durationInSeconds >= 5.0F)
+			stack.damageItem(1, player);
+			world.playSoundAtEntity(player, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+			
+			if (!world.isRemote)
 			{
-				LogHelper.info("Duration in seconds has passed! Firing Entity!");
-				
+				Vec3 look = player.getLookVec();
 				EntityMagicBall magicBall = new EntityMagicBall(world, player);
-	            
-	            stack.damageItem(1, player);
-	            world.playSoundAtEntity(player, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
-	            player.inventory.consumeInventoryItem(ModItems.crystalShard);
-	            
-	            if (!world.isRemote)
-	            {
-	            	world.spawnEntityInWorld(magicBall);
-	            }
+				magicBall.setPosition(player.posX + look.xCoord, player.posY + look.yCoord + 1.5, player.posZ + look.zCoord);
+				player.inventory.consumeInventoryItem(ModItems.crystalShard);
+				world.spawnEntityInWorld(magicBall);
 			}
 		}
 	}
 	
 	@Override
-	public void onPlayerStoppedUsing(ItemStack stack, World world, EntityPlayer player, int par4)
+	public EnumAction getItemUseAction(ItemStack itemstack)
 	{
-		inUse = false;
-	}
-	
-	@Override
-	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
-	{
-		inUse = true;
-		
-		if (player.capabilities.isCreativeMode || player.inventory.hasItem(ModItems.crystalShard))
-		{
-			player.setItemInUse(stack, this.getMaxItemUseDuration(stack));
-		}
-		
-		return stack;
+		return EnumAction.none; //test different actions for better looks
 	}
 	
 	@Override
 	public int getMaxItemUseDuration(ItemStack stack)
     {
-        return 80;
+        return 72000;
     }
 	
 	@Override
-	public void onUpdate(ItemStack stack, World world, Entity entity, int par4, boolean par5)
+	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
 	{
-		EntityPlayer player = (EntityPlayer) entity;
-		
-		//if (player.inventory.getCurrentItem() != null && player.inventory.hasItem(ModArmory.infernoStaff) && player.isUsingItem())
-		//{
-		//	  Only needed for registering icons for level of charge
-		//}
-		
-		if (inUse == true)
+		if (player.inventory.hasItem(ModItems.crystalShard))
 		{
-			chargeStaff(stack, world, player, 20);
+			player.setItemInUse(stack, this.getMaxItemUseDuration(stack));
 		}
+		
+		return stack;
 	}
 }
