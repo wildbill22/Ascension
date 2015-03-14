acdc8fbe037410372a68bc6494954edf10b7b41f
TheXFactor117 <thexfactor117.mods@gmail.com>
2015-03-14 16:53:29 -0500

    Switching workspaces

---
+++ src/main/java/com/thexfactor117/ascension/entities/hostile/EntityGoblin.java
@@ -0,0 +1,121 @@
+package com.thexfactor117.ascension.entities.hostile;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.IRangedAttackMob;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIArrowAttack;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+
+import com.thexfactor117.ascension.entities.projectiles.EntitySmallMagic;
+import com.thexfactor117.ascension.handlers.ConfigHandler;
+
+/**
+ * 
+ * @author TheXFactor117
+ *
+ */
+
+public class EntityGoblin extends EntityMob implements IRangedAttackMob
+{
+	public EntityGoblin(World world) 
+	{
+		super(world);
+		this.getNavigator().setCanSwim(true);
+		this.getNavigator().setAvoidsWater(true);
+		this.experienceValue = 20;
+		this.setSize(0.8F, 2.0F);
+		this.clearAITasks();
+        this.tasks.addTask(0, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+		this.tasks.addTask(1, new EntityAIWander(this, 1.0D));
+		this.tasks.addTask(2, new EntityAISwimming(this));
+		this.tasks.addTask(3, new EntityAILookIdle(this));
+		this.tasks.addTask(4, new EntityAIArrowAttack(this, this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).getAttributeValue(), 20, 64.0F));
+        this.targetTasks.addTask(0, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+	}
+
+	@Override
+	protected void applyEntityAttributes()
+	{
+		super.applyEntityAttributes();
+		this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(ConfigHandler.bansheeFollowRange);
+		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(ConfigHandler.bansheeSpeed);
+		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(ConfigHandler.bansheeHealth);
+	}
+	
+	/**
+	 * Called to perform a ranged attack instead.
+	 */
+	@Override
+	public void attackEntityWithRangedAttack(EntityLivingBase entity, float f) 
+	{
+		EntitySmallMagic magic = new EntitySmallMagic(this.worldObj, this);
+		/** 
+		 * sets the x, y, and z positions of the projectile
+		 * based on the two entities (Attacker, Enemy) positions 
+		 */
+		double x = entity.posX - this.posX;
+		double y = entity.posY + entity.getEyeHeight() - 1.1D - magic.posY;
+		double z = entity.posZ - this.posZ;
+		/**
+		 * Square root to add height to the projectile. Makes the
+		 * projectile sorta "arch". Taken from EntityArrow.
+		 */
+		double i = (double)MathHelper.sqrt_double(x * x + z * z);
+		float j = (float)i * 0.2F;
+		/**
+		 * sets the direction of the projectile, along with the speed, and spread.
+		 */
+		magic.setThrowableHeading(x, y + (double)j, z, 1.0F, 9.0F);
+		
+		this.playSound("random.bow", 1.0F, 1.0F / (this.getRNG().nextFloat() * 0.4F + 0.8F));
+        
+		if (!this.worldObj.isRemote)
+		{
+			this.worldObj.spawnEntityInWorld(magic);
+		}
+	}
+	
+	@Override
+	public boolean isAIEnabled()
+	{
+		return true;
+	}
+	
+	@Override
+	public boolean canDespawn()
+	{
+		return true;
+	}
+	
+	/**
+	 * Clears previous AI Tasks, so the ones defined above will
+	 * actually perform.
+	 */
+	protected void clearAITasks()
+	{
+		tasks.taskEntries.clear();
+		targetTasks.taskEntries.clear();
+	}
+	
+	/**
+	 * Testing purposes. Entities are having trouble attacking
+	 * players; hopefully this will help out.
+	 */
+    @Override
+	protected Entity findPlayerToAttack()
+    {
+        EntityPlayer entityplayer = this.worldObj.getClosestVulnerablePlayerToEntity(this, ConfigHandler.bansheeFollowRange);
+        return entityplayer != null && this.canEntityBeSeen(entityplayer) ? entityplayer : null;
+    }
+}
