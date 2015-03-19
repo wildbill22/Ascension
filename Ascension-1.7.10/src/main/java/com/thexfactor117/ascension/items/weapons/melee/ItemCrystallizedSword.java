5a4470478c97466934e13649ea5303f7bb7f0e37
TheXFactor117 <thexfactor117.mods@gmail.com>
2015-03-19 13:59:15 -0500

    Sorting out a few issues

---
+++ Ascension-1.7.10/src/main/java/com/thexfactor117/ascension/items/weapons/melee/ItemCrystallizedSword.java
@@ -0,0 +1,48 @@
+package com.thexfactor117.ascension.items.weapons.melee;
+
+import java.util.List;
+
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
+
+import com.thexfactor117.ascension.help.Reference;
+import com.thexfactor117.ascension.tabs.ModTabs;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+/**
+ * 
+ * @author TheXFactor117
+ *
+ */
+
+public class ItemCrystallizedSword extends ItemSword
+{
+	public final ToolMaterial toolMaterial;
+	
+	public ItemCrystallizedSword(ToolMaterial EnumToolMaterial)
+	{
+		super(EnumToolMaterial);
+		toolMaterial = EnumToolMaterial;
+		setCreativeTab(ModTabs.tabAscension);
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IIconRegister par1IconRegister)
+	{
+		this.itemIcon = par1IconRegister.registerIcon(Reference.MODID + ":" + getUnlocalizedName().substring(5));
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean par4)
+	{
+		list.add(stack.getMaxDamage() - stack.getItemDamage() + " Hits Remaining");
+		list.add("No Special Abilities");
+	}
+}
